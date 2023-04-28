d937a9b169d60db80cf4165daf8de8db68e45bc3
Rocco Lattanzio <lukelattanzio@yahoo.com>
2023-04-28 16:41:53 -0700

    finished next couple queries

--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/01_queries.rb
@@ -88,6 +88,8 @@ def movie_names_before_1940
   #
   # Use pluck to find the title of all movies made before 1940.
 
-  
-
+  Movie
+    .select(:title)
+    .where('yr < 1940')
+    .pluck(:title)
 end
--- practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
+++ practice-for-ch-sql-movie-buff-long-practice-main/movie_buff/02_queries.rb
@@ -1,18 +1,25 @@
 def eighties_b_movies
   # List all the movies from 1980-1989 with scores falling between 3 and 5
   # (inclusive). Show the id, title, year, and score.
-  
+  Movie
+    .select(:id, :title, :yr, :score)
+    .where('yr BETWEEN 1980 AND 1989')
+    .where(score: 3..5)
+
 end
 
 def bad_years
   # List the years in which no movie with a rating above 8 was released.
-  
+  Movie
+    .select(:yr)
+    .group(:score)
+    .having('MAX(score) <= 8')
 end
 
 def cast_list(title)
   # List all the actors for a particular movie, given the title.
   # Sort the results by starring order (ord). Show the actor id and name.
-  
+
 end
 
 def vanity_projects
@@ -20,11 +27,11 @@ def vanity_projects
   # starring actor. Show the movie id, title, and director's name.
 
   # Note: Directors appear in the 'actors' table.
-  
+
 end
 
 def most_supportive
   # Find the two actors with the largest number of non-starring roles.
   # Show each actor's id, name, and number of supporting roles.
-  
+
 end
